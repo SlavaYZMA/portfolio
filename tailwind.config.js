86a6502c343ec27eb1f078d97838d1d166dd0fe8
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:11:51 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#1a1a1a',
+        'accent-purple': '#ff69b4',
+      },
+    },
+  },
+  plugins: [],
+};
