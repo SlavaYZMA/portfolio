cf5200f6e8b40082f16084b8aa4f7c5a4c193482
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:31:48 +0200

    Update essays.jsx

--- src/pages/[lang]/essays.jsx
+++ src/pages/[lang]/essays.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
+function Essays() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl font-bold mb-4">{t('essays')}</h1>
+      <p className="mb-4">Здесь будут опубликованы мои размышления о связи искусства и науки. Проверяйте обновления!</p>
+    </div>
+  );
+}
+
+export default Essays;
