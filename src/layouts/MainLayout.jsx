787d3496dfe9a867059f54b5704247836bcb75b0
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:29:43 +0200

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+function MainLayout() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default MainLayout;
