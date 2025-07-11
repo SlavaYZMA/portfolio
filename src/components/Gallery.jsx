e63b0bc3a6ab06f489f5777ec2e5f8591270000c
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:25:38 +0200

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
 
+function Gallery({ images }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Project image ${index + 1}`}
+          className="w-full h-64 object-cover rounded"
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
