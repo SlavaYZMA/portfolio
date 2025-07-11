09066d1b7854ea7436ce05cd9137e4b6b1dd7b8d
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:23:37 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,1 +1,42 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "about": "About",
+          "works": "Works",
+          "in-progress": "In Progress",
+          "essays": "Essays",
+          "cv": "CV",
+          "contacts": "Contacts",
+          "privacy-policy": "Privacy Policy",
+          "welcome": "Welcome to Yzma's Portfolio",
+          "description": "A fusion of art and quantum mechanics"
+        }
+      },
+      ru: {
+        translation: {
+          "about": "О проекте",
+          "works": "Работы",
+          "in-progress": "В процессе",
+          "essays": "Эссе",
+          "cv": "Резюме",
+          "contacts": "Контакты",
+          "privacy-policy": "Политика конфиденциальности",
+          "welcome": "Добро пожаловать в портфолио Yzma",
+          "description": "Слияние искусства и квантовой механики"
+        }
+      }
+    },
+    lng: 'en', // Язык по умолчанию
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
