2bb885f5b0e9a171445edd390b0cbf5f2d6945c6
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:23:48 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+import App from './App';
 
+ReactDOM.render(
+  <BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
