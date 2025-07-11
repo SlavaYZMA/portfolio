785fda6a0096bec33b5ec2a378962faf224dd693
SlavaYZMA <slavasolen@gmail.com>
2025-07-11 14:25:25 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
+function Footer() {
+  const { t } = useTranslation();
+
+  return (
+    <footer className="bg-gray-800 p-4 text-center">
+      <p>&copy; 2025 Yzma. All rights reserved.</p>
+      <p>
+        <a href="/privacy-policy" className="text-purple-400 hover:underline">
+          {t('privacy-policy')}
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
